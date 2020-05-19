0a4993c1076806b81466ecd0ee7de9bf9220b73f
Yash agarwal <yash.agarwal1214@gmail.com>
2020-05-19 13:27:55 +0530

    hard Coding it

---
+++ colorgame.js
@@ -0,0 +1,9 @@
+var colors = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)", "rgb(255, 0, 255)", "rgb(0, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickColor = colors[3];
+for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+}
