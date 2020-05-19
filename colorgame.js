01503f440f40f910cb9da0033f50e37edbf25885
Yash agarwal <yash.agarwal1214@gmail.com>
2020-05-19 14:49:50 +0530

    randomizing square Colors

--- colorgame.js
+++ colorgame.js
@@ -1,6 +1,4 @@
-var colors = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)", "rgb(255, 0, 255)", "rgb(0, 0, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -35,4 +33,24 @@ function changeColor(color) {
 function pickColor() {
     var random = Math.floor(Math.random() * colors.length)
     return colors[random];
+}
+
+function generateRandomColors(num) {
+    //make an array 
+    var arr = []
+        // add num random colors to array
+    for (var i = 0; i < num; i++) {
+        //get random color and push them into array
+        arr.push(randomColor())
+    }
+    //return that array
+    return arr;
+
+}
+
+function randomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
 }
