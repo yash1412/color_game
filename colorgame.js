35f6311f1fa28cd234b75a96e1a2654291dae05e
Yash agarwal <yash.agarwal1214@gmail.com>
2020-05-19 15:16:51 +0530

    functioning of the button(New Colors)

--- colorgame.js
+++ colorgame.js
@@ -3,7 +3,24 @@ var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
-var messageDisplay = document.querySelector("#message")
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function() {
+    //generate all new colors
+    colors = generateRandomColors(6);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change color display
+    colorDisplay.textContent = pickedColor;
+    //change colors of squares
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "#232323";
+})
+
 colorDisplay.textContent = pickedColor
 for (var i = 0; i < squares.length; i++) {
     //add initial colors to squares
@@ -14,8 +31,10 @@ for (var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.backgroundColor;
 
         if (clickedColor === pickedColor) {
+            resetButton.textContent = "Play Again?"
             messageDisplay.textContent = "Correct";
             changeColor(clickedColor)
+            h1.style.backgroundColor = pickedColor
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "TryAgain"
