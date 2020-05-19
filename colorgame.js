ffe8d529dfe9558448cb86d8f9aac984f8c404f7
Yash agarwal <yash.agarwal1214@gmail.com>
2020-05-19 13:46:35 +0530

    adding event-alert if the color matches or not

--- colorgame.js
+++ colorgame.js
@@ -3,7 +3,23 @@ var colors = ["rgb(255, 0, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickColor = colors[3];
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+colorDisplay.textContent = pickedColor
 for (var i = 0; i < squares.length; i++) {
+    //add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function() {
+        var clickedColor = this.style.backgroundColor;
+
+        if (clickedColor === pickedColor) {
+            alert("corect!");
+
+        } else {
+            alert("wrong!!");
+        }
+
+    })
 }
